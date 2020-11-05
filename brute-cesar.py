48837f510ced915a2226b1ba316bb60c6aef3924
Gabrielle Liberato <40310291+gabiliberato@users.noreply.github.com>
2020-11-05 13:22:41 -0300

    Add files via upload

---
+++ brute-cesar.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from string import lowercase as lc 
+
+file = open(sys.argv[1], 'r').read().lower() #ARQUIVE
+
+
+for key in xrange(1,26):
+	result = ''
+	print 'Key:', key
+	for lt in file:
+		if lt in lc:
+			idx = lc.find(lt)
+			idx = (idx - key) % 26
+			result+= lc[idx]
+		else:
+			result += lt
+	print result,
+
+
+
---
+++ cesar.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from string import lowercase as lc 
+
+file = open(sys.argv[1], 'r').read().lower() #ARQUIVE
+key = int(sys.argv[2]) #KEY
+mode = sys.argv[3]
+
+result = ''
+
+for lt in file:
+	if lt in lc:
+		idx = lc.find(lt)
+		if mode == 'enc':
+			idx = (idx + key) % 26 # Aritimética modular - caso valores sobrem voltar ao inicio do alfabeto e contar novamente
+		elif mode == 'dec':
+			idx == (idx - key) % 26
+		result+= lc[idx]
+	else:
+		result += lt
+
+
+print result,
+
+
+
